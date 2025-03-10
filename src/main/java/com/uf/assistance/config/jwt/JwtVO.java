fdd91ad46413c11579bb5c46290df7577ef118b6
naegwon <joonwoo9192@gmail.com>
2025-03-10 20:36:37 +0900

    로그인시 Secret 상수 environment에서 가져오게 변경

--- src/main/java/com/uf/assistance/config/jwt/JwtVO.java
+++ src/main/java/com/uf/assistance/config/jwt/JwtVO.java
@@ -1,15 +1,24 @@
 package com.uf.assistance.config.jwt;
 
-public interface JwtVO {
-    //HS256 대칭키  -- 이거는 절대 노출 X 환경 변수나 DB값을 쓸것
-    //TO-DO 추후 변경필요
-    public static final String SECRET = System.getenv("SECRET") != null
-            ? System.getenv("SECRET")
-            : "ERROR";
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
-    public static final int EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7;  //만료시간 1주일
-
+@Component
+public class JwtVO {
+    // 원래 static final 상수들
+    public static String SECRET;
+    public static final int EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7;  // 만료시간 1주일
     public static final String TOKEN_PREFIX = "Bearer ";
-
     public static final String HEADER_STRING = "Authorization";
+
+    // 초기화를 위한 생성자
+    @Autowired
+    public JwtVO(Environment environment) {
+        // SECRET 값을 Environment에서 가져옴
+        SECRET = environment.getProperty("SECRET");
+        if (SECRET == null) {
+            SECRET = "ERROR";
+        }
+    }
 }
