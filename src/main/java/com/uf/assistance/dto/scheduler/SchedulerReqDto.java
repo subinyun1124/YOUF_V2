09121963a96af28fd96f74bc83a3241a933cd20d
Inhyo <izp1012@naver.com>
2025-04-28 02:21:28 +0900

    SchedulerReqDto 수정

--- src/main/java/com/uf/assistance/dto/scheduler/SchedulerReqDto.java
+++ src/main/java/com/uf/assistance/dto/scheduler/SchedulerReqDto.java
@@ -8,18 +8,23 @@ public class SchedulerReqDto {
 
     private String jobName;
     private String jobGroup;
+    private String jobClass;
     private String cronExpression;
+    private String description;
     private String jobType;
     private String jobData;
+    private ScheduledJob.Status status;
 
     public static ScheduledJob toEntity(SchedulerReqDto schedulerReqDto) {
         return ScheduledJob.builder()
                 .jobName(schedulerReqDto.getJobName())
                 .jobGroup(schedulerReqDto.getJobGroup() != null ? schedulerReqDto.getJobGroup() : "DEFAULT")
+                .jobClass(schedulerReqDto.getJobClass())
                 .cronExpression(schedulerReqDto.getCronExpression())
+                .description(schedulerReqDto.getDescription())
                 .jobType(schedulerReqDto.getJobType())
                 .jobData(schedulerReqDto.jobData)
-                .status(ScheduledJob.Status.ENABLED)
+                .status(ScheduledJob.Status.NEW)
                 .build();
     }
 }
